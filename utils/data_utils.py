7b1e9bad068c8d70f085dff608c5f14498a68bbc
Matt Mollison <matt.mollison@gmail.com>
2020-09-13 15:03:38 -0700

    docstring and type hints

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,5 +1,22 @@
 from itertools import islice
+from typing import List, Tuple
 
 
-def n_wise(iterable, n):
+def n_wise(iterable: List, n: int) -> zip(Tuple):
+    """n_wise - Given an iterable, create a generator of successive groups of size n
+
+    list(n_wise([1, 2, 3, 4, 5], 3)) -> [(1, 2, 3), (2, 3, 4), (3, 4, 5)]
+
+    Parameters
+    ----------
+    iterable : List (or any iterable)
+        Items to include in groups
+    n : int
+        Group size
+
+    Returns
+    -------
+    zip generator of tuples
+        Items in groups
+    """
     return zip(*(islice(iterable, i, None) for i in range(n)))
