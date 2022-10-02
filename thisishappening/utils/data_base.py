b41586d77fc64afd031def4af0bc6978ffea6df6
Matt Mollison <matt.mollison@gmail.com>
2022-10-02 12:26:48 -0400

    Add retry, infrastructure rearrangement (#30)
    * add tenacity and poetry update
    
    * no need to import from typing
    
    * bump poetry version to 1.2.1
    
    * simplify
    
    * set up exceptions
    
    * rename session to db_session
    
    * rename session to db_session
    
    * reconfigure classes
    
    * check correct file
    
    * correct return type

--- thisishappening/utils/data_base.py
+++ thisishappening/utils/data_base.py
@@ -1,6 +1,5 @@
 import logging
 from datetime import datetime, timedelta
-from typing import List
 
 import pytz
 from sqlalchemy import (
@@ -55,7 +54,7 @@ class Events(Base):
     event_type = Column(String, nullable=True)
 
     @classmethod
-    def log_event(cls, session, event_info: EventInfo):
+    def log_event(cls, db_session, event_info: EventInfo):
         # Add to events table
         event = cls(
             timestamp=event_info.timestamp,
@@ -71,9 +70,9 @@ class Events(Base):
             status_ids=event_info.status_ids,
             event_type=event_info.event_type,
         )
-        session.add(event)
+        db_session.add(event)
         try:
-            session.commit()
+            db_session.commit()
             logger.info(
                 f"Logged event: {event_info.timestamp} {event_info.place_name}:"
                 f" {event_info.tokens_str}"
@@ -84,24 +83,24 @@ class Events(Base):
                 f" {event_info.timestamp} {event_info.place_name}:"
                 f" {event_info.tokens_str}: {e}"
             )
-            session.rollback()
+            db_session.rollback()
 
         return event
 
     @classmethod
     def get_recent_events(
         cls,
-        session,
+        db_session,
         timestamp: datetime = None,
         hours: float = 1,
-        event_type: List[str] = None,
+        event_type: list[str] = None,
     ):
         timestamp = timestamp or datetime.utcnow().replace(tzinfo=pytz.UTC)
 
         ts_start = timestamp - timedelta(hours=hours)
 
         q = (
-            session.query(cls)
+            db_session.query(cls)
             .filter(cls.timestamp >= ts_start)
             .filter(cls.timestamp <= timestamp)
         )
@@ -117,8 +116,8 @@ class Events(Base):
         return q.order_by(desc(cls.timestamp)).all()
 
     @classmethod
-    def get_most_recent_event(cls, session, event_type: List[str] = None):
-        q = session.query(cls)
+    def get_most_recent_event(cls, db_session, event_type: list[str] = None):
+        q = db_session.query(cls)
 
         if event_type is not None:
             q = q.filter(
@@ -131,9 +130,9 @@ class Events(Base):
         return q.order_by(desc(cls.timestamp)).first()
 
     @classmethod
-    def get_event_tweets(cls, session, event_id: int, hours: float = 1):
+    def get_event_tweets(cls, db_session, event_id: int, hours: float = 1):
         event = (
-            session.query(cls)
+            db_session.query(cls)
             .filter(cls.id == event_id)
             .order_by(desc(cls.timestamp))
             .first()
@@ -147,7 +146,7 @@ class Events(Base):
                 event.north_lat,
             ]
             event_tweets = RecentTweets.get_recent_tweets(
-                session, timestamp=timestamp, hours=hours, bounding_box=bounding_box
+                db_session, timestamp=timestamp, hours=hours, bounding_box=bounding_box
             )
         else:
             logger.info(f"Event ID {event_id} not found")
@@ -161,7 +160,7 @@ class Events(Base):
     @classmethod
     def delete_events_older_than(
         cls,
-        session,
+        db_session,
         timestamp: datetime = None,
         hours: float = None,
         days: float = None,
@@ -183,20 +182,20 @@ class Events(Base):
                     f" {hours} hours {days} days {weeks} weeks"
                 )
                 delete_q = cls.__table__.delete().where(cls.timestamp < ts_end)
-                session.execute(delete_q)
-                session.commit()
+                db_session.execute(delete_q)
+                db_session.commit()
             except Exception as e:
                 logger.warning(
                     f"Exception when deleting events older than {ts_end}:"
                     f" {hours} hours {days} days {weeks} weeks: {e}"
                 )
-                session.rollback()
+                db_session.rollback()
 
     @classmethod
-    def keep_events_n_rows(cls, session, n: int = None):
+    def keep_events_n_rows(cls, db_session, n: int = None):
         """Keep the most recent n rows"""
         if n is not None:
-            ids = session.query(cls.id).order_by(desc(cls.timestamp)).all()
+            ids = db_session.query(cls.id).order_by(desc(cls.timestamp)).all()
             ids_to_delete = [x[0] for x in ids[n:]]
 
             if ids_to_delete:
@@ -204,13 +203,13 @@ class Events(Base):
                     logger.info(f"Keeping most recent {n} rows of events")
                     delete_q = cls.__table__.delete().where(cls.id.in_(ids_to_delete))
 
-                    session.execute(delete_q)
-                    session.commit()
+                    db_session.execute(delete_q)
+                    db_session.commit()
                 except Exception as e:
                     logger.warning(
                         f"Exception when keeping most recent {n} rows of events: {e}"
                     )
-                    session.rollback()
+                    db_session.rollback()
 
 
 class RecentTweets(Base):
@@ -237,7 +236,7 @@ class RecentTweets(Base):
     place_type = Column(String, nullable=True)
 
     @classmethod
-    def log_tweet(cls, session, tweet_info: TweetInfo):
+    def log_tweet(cls, db_session, tweet_info: TweetInfo):
         tweet = cls(
             status_id_str=tweet_info.status_id_str,
             user_screen_name=tweet_info.user_screen_name,
@@ -256,9 +255,9 @@ class RecentTweets(Base):
             place_name=tweet_info.place_name,
             place_type=tweet_info.place_type,
         )
-        session.add(tweet)
+        db_session.add(tweet)
         try:
-            session.commit()
+            db_session.commit()
             logger.info(
                 f"Logged tweet: {tweet_info.status_id_str},"
                 f" coordinates: ({tweet_info.latitude}, {tweet_info.longitude}),"
@@ -270,24 +269,24 @@ class RecentTweets(Base):
                 f" coordinates: ({tweet_info.latitude}, {tweet_info.longitude}),"
                 f" {tweet_info.place_name} ({tweet_info.place_type}): {e}"
             )
-            session.rollback()
+            db_session.rollback()
 
         return tweet
 
     @classmethod
     def count_tweets(
         cls,
-        session,
+        db_session,
         timestamp: datetime = None,
         hours: float = 0,
-        bounding_box: List[float] = None,
+        bounding_box: list[float] = None,
     ):
         timestamp = timestamp or datetime.utcnow().replace(tzinfo=pytz.UTC)
 
         ts_start = timestamp - timedelta(hours=hours)
 
         q = (
-            session.query(cls, func.count(cls.status_id_str))
+            db_session.query(cls, func.count(cls.status_id_str))
             .filter(cls.created_at >= ts_start)
             .filter(cls.created_at <= timestamp)
         )
@@ -308,11 +307,11 @@ class RecentTweets(Base):
     @classmethod
     def get_recent_tweets(
         cls,
-        session,
+        db_session,
         timestamp: datetime = None,
         hours: float = 1,
-        bounding_box: List[float] = None,
-        place_type: List[str] = None,
+        bounding_box: list[float] = None,
+        place_type: list[str] = None,
         has_coords: bool = None,
         place_type_or_coords: bool = True,
         include_quote_status: bool = True,
@@ -324,7 +323,7 @@ class RecentTweets(Base):
         ts_start = timestamp - timedelta(hours=hours)
 
         q = (
-            session.query(cls)
+            db_session.query(cls)
             .filter(cls.created_at >= ts_start)
             .filter(cls.created_at <= timestamp)
         )
@@ -370,8 +369,8 @@ class RecentTweets(Base):
         return q.order_by(desc(cls.created_at)).all()
 
     @classmethod
-    def get_oldest_tweet(cls, session, bounding_box: List[float] = None):
-        q = session.query(cls)
+    def get_oldest_tweet(cls, db_session, bounding_box: list[float] = None):
+        q = db_session.query(cls)
 
         if bounding_box is not None:
             west_lon, east_lon, south_lat, north_lat = get_coords_min_max(
@@ -387,8 +386,8 @@ class RecentTweets(Base):
         return q.order_by(cls.created_at).first()
 
     @classmethod
-    def get_most_recent_tweet(cls, session, bounding_box: List[float] = None):
-        q = session.query(cls)
+    def get_most_recent_tweet(cls, db_session, bounding_box: list[float] = None):
+        q = db_session.query(cls)
 
         if bounding_box is not None:
             west_lon, east_lon, south_lat, north_lat = get_coords_min_max(
@@ -404,21 +403,21 @@ class RecentTweets(Base):
         return q.order_by(desc(cls.created_at)).first()
 
     @classmethod
-    def update_tweet_deleted(cls, session, status_id_str: str):
+    def update_tweet_deleted(cls, db_session, status_id_str: str):
         """Mark tweet as deleted"""
         try:
-            session.query(cls).filter(cls.status_id_str == status_id_str).update(
+            db_session.query(cls).filter(cls.status_id_str == status_id_str).update(
                 {"deleted_at": datetime.utcnow().replace(tzinfo=pytz.UTC)}
             )
-            session.commit()
+            db_session.commit()
         except Exception as e:
             logger.warning(f"Exception when updating tweet as deleted: {e}")
-            session.rollback()
+            db_session.rollback()
 
     @classmethod
     def delete_tweets_older_than(
         cls,
-        session,
+        db_session,
         timestamp: datetime = None,
         hours: float = None,
         days: float = None,
@@ -440,20 +439,20 @@ class RecentTweets(Base):
                     f" {hours} hours {days} days {weeks} weeks"
                 )
                 delete_q = cls.__table__.delete().where(cls.created_at < ts_end)
-                session.execute(delete_q)
-                session.commit()
+                db_session.execute(delete_q)
+                db_session.commit()
             except Exception as e:
                 logger.warning(
                     f"Exception when deleting tweets older than {ts_end}:"
                     f" {hours} hours {days} days {weeks} weeks: {e}"
                 )
-                session.rollback()
+                db_session.rollback()
 
     @classmethod
-    def keep_tweets_n_rows(cls, session, n: int = None):
+    def keep_tweets_n_rows(cls, db_session, n: int = None):
         """Keep the most recent n rows"""
         if n is not None:
-            ids = session.query(cls.id).order_by(desc(cls.created_at)).all()
+            ids = db_session.query(cls.id).order_by(desc(cls.created_at)).all()
             ids_to_delete = [x[0] for x in ids[n:]]
 
             if ids_to_delete:
@@ -461,10 +460,10 @@ class RecentTweets(Base):
                     logger.info(f"Keeping most recent {n} rows of tweets")
                     delete_q = cls.__table__.delete().where(cls.id.in_(ids_to_delete))
 
-                    session.execute(delete_q)
-                    session.commit()
+                    db_session.execute(delete_q)
+                    db_session.commit()
                 except Exception as e:
                     logger.warning(
                         f"Exception when keeping most recent {n} rows of tweets: {e}"
                     )
-                    session.rollback()
+                    db_session.rollback()
